f5db8a6191dcce5836d4e84da857631e3daf2838
btsznh <btsznh@gmail.com>
2022-08-17 21:01:21 +0530

    divide and multiply

--- add.py
+++ add.py
@@ -4,13 +4,18 @@ def barathAdd(x,y):
 def barathSub(x,y):
     return x - y
 
+def multiply(x, y):
+    return x * y
 
+# This function divides two numbers
+def divide(x, y):
+    return x / y
 
 while True:
 
-    choice = input("Enter Choice 1/2 = ")
+    choice = input("Enter Choice 1/2/3/4 = ")
 
-    if choice in ('1','2'):
+    if choice in ('1','2', '3', '4'):
         print("condition 1")
         x = float(input("Enter first number: "))
         y = float(input("Enter second number: "))
@@ -22,6 +27,14 @@ while True:
         elif choice == '2':
             print("condition 3")
             print(barathSub(x,y))
+
+        elif choice == '3':
+            print("condition 3")
+            print(multiply(x,y))
+
+        elif choice == '4':
+            print("condition 3")
+            print(divide(x,y))
 
         next_calculation = input("Another Calculation (yes/no)")
         if next_calculation == 'no':
